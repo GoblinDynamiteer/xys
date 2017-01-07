13ab055de777d3fcb4c2db9c33cef970fa2b7ce0
Johan Kämpe <kampe.johan@gmail.com>
2017-01-07 12:55:16 +0100

    Script now handles single search results.
    Decoded JSON has list/dict depending on number of serach results.

--- python/unc_search.py
+++ python/unc_search.py
@@ -7,9 +7,16 @@ url = sys.argv[1]
 jsonResponse = urllib.request.urlopen(url).read().decode('utf-8')
 decodedJson = json.loads(jsonResponse) 
 
-numberItems = len(decodedJson["channel"]["item"])
+#returned several search hits
+if isinstance(decodedJson["channel"]["item"], list):
+	numberItems = len(decodedJson["channel"]["item"])
+	for n in range(0,numberItems):
+		print(decodedJson["channel"]["item"][n]["title"], ";", decodedJson["channel"]["item"][n]["link"])
+		if n != numberItems:
+			print("|") #for list separator in xyplorer script
 
-for n in range(0,numberItems):
-	print(decodedJson["channel"]["item"][n]["title"], ";", decodedJson["channel"]["item"][n]["link"])
-	if n != numberItems:
-		print("|") #for list separator in xyplorer script
+#returned only one search hit
+elif  isinstance(decodedJson["channel"]["item"], dict):
+	print(decodedJson["channel"]["item"]["title"], ";", decodedJson["channel"]["item"]["link"])
+
+
